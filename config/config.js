8a843c2e93929a528b76c684dd520bb393f6d72c
ThangNguyenTan <85928224+ThangNguyenTan@users.noreply.github.com>
2023-02-18 20:02:34 +0700

    fix: update config vars

--- config/config.js
+++ config/config.js
@@ -4,7 +4,11 @@ module.exports = {
   development: config.get('database'),
   test: config.get('database_test'),
   production: {
-    ...config.get('database'),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION,
     dialectOptions: {
       ssl: {
         require: true,
